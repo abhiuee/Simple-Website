5917d4b5334a4499450a1ad47411f29669bc83a3
Abhishek <abhishek.28s@gmail.com>
2015-12-15 00:59:45 -0800

    Added error js

--- bootstrap/js/location.js
+++ bootstrap/js/location.js
@@ -6,15 +6,25 @@ function clicked(cb) {
 		console.log("Checked")
 		if (navigator.geolocation) {
 			console.log("Navigator location")
-			navigator.geolocation.getCurrentPosition(showPosition);
+			navigator.geolocation.getCurrentPosition(showPosition, geoError);
 		} 
 	}
 }
+
+var geoError = function(error) {
+    console.log('Error occurred. Error code: ' + error.code);
+    // error.code can be:
+    //   0: unknown error
+    //   1: permission denied
+    //   2: position unavailable (error response from location provider)
+    //   3: timed out
+};
 
 function showPosition(position) {
 	//Get latitude and longiture and fill in the hidden input form
 	var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
     var b = document.getElementById("hidden_location");
+    console.log(latitude)
     b.value = latitude+","+longitude;
 }
